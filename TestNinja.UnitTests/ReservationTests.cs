9677fcbb9f093b6e4bfae47323db94e4a77536d9
Tiermed1! <bhattp2>
2019-10-25 11:31:01 -0700

    FirstCommit

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var resservation = new Reservation();
+
+            //Act
+            var result = resservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCanncelledBy_UserIsMadeBy_ReturnsTrue()
+        {
+            //Arrange
+            var user = new User();
+            var resservation = new Reservation { MadeBy = user};
+
+            //Act
+            var result = resservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNewuser_ReturnsFalse()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = new User() };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //
+            Assert.IsFalse(result);
+        }
+    }
+}
