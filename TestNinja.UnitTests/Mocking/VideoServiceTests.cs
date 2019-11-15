b4d94066c87729908c8c34f6e6675fedc5dd1384
Pooja Bhatt <pooja.bhatt@unlv.edu>
2019-11-15 14:27:29 -0800

    Dependency Injection through constructor

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -11,9 +11,7 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
-            service.FileReader = new FakeFileReader();
-
+            var service = new VideoService(new FakeFileReader());
             var result = service.ReadVideoTitle();
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
