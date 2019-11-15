6187df2865d013634e8e5221e1ba588a02d40e30
Pooja Bhatt <pooja.bhatt@unlv.edu>
2019-11-15 15:50:53 -0800

    Added EmployeeController and Installer Helper Unit Tests

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<String>(), It.IsAny<String>())).Throws<WebException>();
+            var result = _installHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Data.Entity;
+using Moq;
 
 namespace TestNinja.Mocking
 {
     public class EmployeeController
     {
-        private EmployeeContext _db;
+        private readonly IEmployeeStorage _storage;
+        private Mock<IEmployeeStorage> storage;
 
-        public EmployeeController()
+        public EmployeeController(IEmployeeStorage storage)
         {
-            _db = new EmployeeContext();
+            _storage = storage;
+  
+        }
+
+        public EmployeeController(Mock<IEmployeeStorage> storage)
+        {
+            this.storage = storage;
         }
 
         public ActionResult DeleteEmployee(int id)
         {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+            _storage.DeleteEmployee(id);
             return RedirectToAction("Employees");
         }
 
